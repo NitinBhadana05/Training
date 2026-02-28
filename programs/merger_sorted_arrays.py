364871f8d5de4886ee831b146dd37ce3dc1450af
Nitin Bhadana <iamnitin931@gmail.com>
2026-02-28 12:26:59 +0530

    Implement a function to merge two sorted arrays.

---
+++ programs/merger_sorted_arrays.py
@@ -0,0 +1,34 @@
+def merge_sorted(arr1: list[int], arr2: list[int]) -> list[int]:
+
+    i: int = 0
+    j: int = 0
+
+    merged: list[int] = []
+
+    while i < len(arr1) and j < len(arr2):
+
+        if arr1[i] <= arr2[j]:
+            merged.append(arr1[i])
+            i += 1
+        else:
+            merged.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        merged.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        merged.append(arr2[j])
+        j += 1
+
+    return merged
+
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+
+print(merge_sorted(arr1, arr2))
+
+"""
+Output: [1, 2, 3, 4, 5, 6]
+"""
