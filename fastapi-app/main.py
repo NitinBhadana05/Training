5107c0414f724697f1cc0d4f685f0c9349a9e814
Nitin Bhadana <iamnitin931@gamil.com>
2026-03-23 14:45:26 +0530

    pratice  Path and Query peramaters

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -26,3 +26,28 @@ def update_item(item_id: int):
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
     return {"message": f"Item {item_id} deleted"}
+    
+#pratice Path parameters and Quarry parameters 
+@app.get("/student/{id}")
+def get_stsudent(id: int):
+    return {"id":id}
+    
+@app.get("/search")
+def search(name: str):
+    return {"name": name}
+    
+@app.get("/users/{user_id}")
+def get_user(user_id: int, is_active: bool):
+    return({"user_id": user_id, "active": is_active})
+    
+@app.get("/car/{id}")  
+def get_car(id: int):
+    return {"id": id}
+
+@app.get("/fillter")
+def get_price(price: int):
+    return {"price":price}
+    
+@app.get("/orders/{order_id}")
+def  get_order(order_id: int, status: str):
+    return {"order_id": order_id,"status": status}
