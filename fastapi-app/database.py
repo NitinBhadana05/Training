c9e4fafdbeaf76f249e9884299985bf86b25583e
Nitin Bhadana <iamnitin931@gamil.com>
2026-03-24 15:22:24 +0530

    feat: add PostgreSQL database integration with SQLAlchemy

---
+++ fastapi-app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+
+
+DATABASE_URL = "postgresql://postgres:1234@localhost/fastapi_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.routes import user,product
+
+from sqlalchemy.orm import Session
+from database import get_db
+import models
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(product.router)
+
+
+
+
+
+@app.post("/users")
+def create_user(name: str, age: int, db: Session = Depends(get_db)):
+    user = models.User(name=name, age=age)
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
