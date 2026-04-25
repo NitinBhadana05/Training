8936d9bc3e8d40d68a11362bf44aa620d9201cfa
Nitin Bhadana <iamnitin931@gamil.com>
2026-04-25 16:00:07 +0530

    feat: implement complete controllers for authentication and library system
    - Added UsersController for signup and user management
    - Implemented SessionsController for login/logout functionality
    - Created BooksController for book operations
    - Built IssuesController for issuing and returning books
    - Integrated authentication checks and flash messaging

--- assignment/rails_library_management_system/library_management/app/controllers/issues_controller.rb
+++ assignment/rails_library_management_system/library_management/app/controllers/issues_controller.rb
@@ -1,7 +1,32 @@
 class IssuesController < ApplicationController
   def new
+    @issue = Issue.new
   end
 
   def create
+    @issue = Issue.new(issue_params)
+
+    if @issue.save
+      redirect_to books_path, notice: "Book issued successfully"
+    else
+      flash.now[:alert] = @issue.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def return_book
+    @issue = Issue.find(params[:id])
+
+    if @issue.update(return_date: Date.today)
+      redirect_to books_path, notice: "Book returned successfully"
+    else
+      redirect_to books_path, alert: "Failed to return book"
+    end
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:user_id, :book_id, :issue_date)
   end
 end
--- assignment/rails_library_management_system/library_management/app/controllers/users_controller.rb
+++ assignment/rails_library_management_system/library_management/app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to books_path, notice: "Account created successfully"
+    else
+      flash.now[:alert] = "Signup failed"
+      render :new
+    end
   end
 
   def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
